b7226e94fdf9e39d3301f5d68c679d1410917c8c
ldarriba <ldplaura@gmail.com>
2021-07-23 11:50:45 +0200

    clenaning of eliminate_duplicates.py with pylint

--- workflow/scripts/eliminate_duplicates.py
+++ workflow/scripts/eliminate_duplicates.py
@@ -1,47 +1,137 @@
+# This file is part of Hi-FRIENDS SDC2
+# (https://github.com/HI-FRIENDS-SDC2/hi-friends).
+# Copyright (c) 2021 Javier Moldón
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+This script removes duplicates and creates a catalog without duplicated sources
+'''
+
 import argparse
 import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy import units as u
-from astropy.coordinates import match_coordinates_sky
 from astropy.table import Table
-from astropy.io import fits
 
 def get_args():
     '''This function parses and returns arguments passed in'''
     # Assign description to the help doc
     description = 'Eliminate duplicates from catalog for sources in the overlapping regions'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-i', '--infile', dest='infile', help='catalog with duplicated sources')
-    parser.add_argument('-o', '--outfile', dest='outfile', help='catalog without duplicated sources')
+    parser.add_argument('-i', '--infile', dest='infile', \
+                        help='catalog with duplicated sources')
+    parser.add_argument('-o', '--outfile', dest='outfile',\
+                        help='catalog without duplicated sources')
     args = parser.parse_args()
     return args
 
 def read_ref_catalog(infile, name_list):
-    catalog_table = Table.read(infile, format='ascii', delimiter=' ', names=name_list)
+    '''Reads the catalog with the variables from an input string
+    Parameters
+    ----------
+    infile: str
+        Input file name
+    name_list: str
+        List of varible names
+    Returns
+    -------
+    catalog_table: object***
+    Examples
+    --------
+    ****
+    '''
+    catalog_table = Table.read(infile, format='ascii', delimiter=' ', \
+                               names=name_list)
     return catalog_table
 
 def read_coordinates_from_table(cat):
-    ra = cat['ra']*u.degree
-    de = cat['dec']*u.degree
+    '''Reads coordinates from a table
+    Parameters
+    ----------
+    cat: ****
+        ****
+    Returns
+    -------
+    ras: float
+        Right ascension
+    dec: float
+        Declination
+    freq: float
+        Frequency
+    Examples
+    --------
+    ****
+    '''
+    ras = cat['ra']*u.degree
+    dec = cat['dec']*u.degree
     freq = cat['central_freq']*u.Hz
-    return ra, de, freq
+    return ras, dec, freq
 
-def find_catalog_duplicates(ra, de, freq):
-    c = SkyCoord(ra=ra, dec=de)
-    idx, d2d, d3d = c.match_to_catalog_sky(c, nthneighbor=2)
+def find_catalog_duplicates(ras, dec, freq):
+    '''Finds duplicates in the catalog
+    Parameters
+    ----------
+    ras: float
+        Right ascension
+    dec: float
+        Declination
+    freq: float
+        Frequency
+    Returns
+    -------
+    cond: array
+        *****
+    idx: array****
+         Index ****
+    Examples
+    --------
+    ****
+    '''
+    coord = SkyCoord(ra=ras, dec=dec)
+    idx, d2d, _ = coord.match_to_catalog_sky(coord, nthneighbor=2)
     max_sep = 3 * u.arcsec
     sep_constraint = d2d < max_sep
     max_freq = 20 * u.MHz
     freq_sep = abs(freq - freq[idx])
     freq_constraint = freq_sep < max_freq
     cond = sep_constraint * freq_constraint
-    c_matches = c[cond]
+    #c_matches = coord[cond]
     return cond, idx
 
 def mask_worse_duplicates(cond, idx, catalog_table):
+    '''Finds worse duplicates and masks them
+    Parameters
+    ----------
+    con: ****
+        ****
+    idx: array****
+        Index of ****
+    catalog_table: astropy table
+        ****
+    Returns
+    -------
+    duplicates: array
+        Array containing the duplicates. True means it is a worse duplicate.
+        False means it is not a worse duplicate.
+    Examples
+    --------
+    ****
+    '''
     duplicates = np.copy(cond)
-    print(f'id1 subcube1 id_subcube1 rms1 id2 subcube2 id_subcube2 rms2')
-    # Here we are counting all matches twice. We could directly use the first half of the matches
+    print('id1 subcube1 id_subcube1 rms1 id2 subcube2 id_subcube2 rms2')
+    # Here we are counting all matches twice.
+    #We could directly use the first half of the matches
     for i in np.where(cond)[0]:
         id1 = catalog_table[idx[i]]['id_subcube']
         id2 = catalog_table[i]['id_subcube']
@@ -59,20 +149,23 @@ def mask_worse_duplicates(cond, idx, catalog_table):
 
 
 def main():
+    '''Removes duplicates and creates a catalog without duplicated sources'''
     args = get_args()
-    name_list = ['id_subcube', 'ra', 'dec', 'hi_size', 'line_flux_integral', 'central_freq', 'pa', 'i', 'w20', 'rms', 'subcube']
+    name_list = ['id_subcube', 'ra', 'dec', 'hi_size', 'line_flux_integral', \
+                 'central_freq', 'pa', 'i', 'w20', 'rms', 'subcube']
     catalog_table = read_ref_catalog(args.infile, name_list=name_list)
-    ra, de, freq = read_coordinates_from_table(catalog_table)
-    cond, idx = find_catalog_duplicates(ra, de, freq)
+    ras, dec, freq = read_coordinates_from_table(catalog_table)
+    cond, idx = find_catalog_duplicates(ras, dec, freq)
     duplicates = mask_worse_duplicates(cond, idx, catalog_table)
     final_table = catalog_table[~duplicates][name_list[:-1]]
     final_table.sort(['ra', 'dec'])
     final_table['id'] = range(len(final_table))
-    name_list_out = ['id', 'ra', 'dec', 'hi_size', 'line_flux_integral', 'central_freq', 'pa', 'i', 'w20']
+    name_list_out = ['id', 'ra', 'dec', 'hi_size', 'line_flux_integral', \
+                     'central_freq', 'pa', 'i', 'w20']
     final_table = final_table[name_list_out]
-    final_table.write(args.outfile, names=name_list_out, format='ascii', overwrite=True)
+    final_table.write(args.outfile, names=name_list_out, format='ascii', \
+                      overwrite=True)
 
 
 if __name__ == '__main__':
     main()
-
