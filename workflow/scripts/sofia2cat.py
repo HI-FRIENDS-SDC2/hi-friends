6b2324554332a368a220ab119cbb356d2a999e1e
ldarriba <ldplaura@gmail.com>
2021-07-30 11:33:47 +0200

    Cleaning with pylint

--- workflow/scripts/sofia2cat.py
+++ workflow/scripts/sofia2cat.py
@@ -1,9 +1,30 @@
+# This file is part of Hi-FRIENDS SDC2
+# (https://github.com/HI-FRIENDS-SDC2/hi-friends).
+# Copyright (c) 2021 Javier Moldón
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+This script defines ****
+'''
+
+
 import os
 import argparse
 import subprocess
+import re
 import pandas as pd
 import numpy as np
-import re
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy import constants as const
@@ -16,10 +37,16 @@ def get_args():
     # Assign description to the help doc
     description = 'Select dataset'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-r', '--results_path', dest='results_path', help='Directory for results', default='results')
-    parser.add_argument('-o', '--outname', dest='outname', help='Name of output directory for Sofia products', default='test')
-    parser.add_argument('-d', '--datacube', dest='datacube', help='Data cube to process. Options are: development, development_large, evaluation', default='development')
-    parser.add_argument('-i', '--incatalog', dest='incatalog', help='Path of the input Sofia catalog to be converted', default='development')
+    parser.add_argument('-r', '--results_path', dest='results_path', \
+                        help='Directory for results', default='results')
+    parser.add_argument('-o', '--outname', dest='outname', help='Name of \
+                        output directory for Sofia products', default='test')
+    parser.add_argument('-d', '--datacube', dest='datacube', help='Data cube \
+                        to process. Options are: development, development_large,\
+                        evaluation', default='development')
+    parser.add_argument('-i', '--incatalog', dest='incatalog', help='Path of \
+                        the input Sofia catalog to be converted', \
+                        default='development')
     args = parser.parse_args()
     return args
 
@@ -31,21 +58,22 @@ def read_sofia_header(filename):
 
 def sofia2cat(catalog):
     head = read_sofia_header(catalog)
-    raw_cat = pd.read_csv(catalog, delim_whitespace=True, header=None, names=head, comment='#')
+    raw_cat = pd.read_csv(catalog, delim_whitespace=True, header=None, \
+                          names=head, comment='#')
     raw_cat.sort_values(by='f_sum', ascending=False, inplace=True)
     raw_cat_filtered = raw_cat[raw_cat['kin_pa']>0]
     print('Producing sofia raw catalog filtered by kin_pa > 0:')
     return raw_cat_filtered
 
-def pix2coord(wcs, x, y):
-    coord = wcs.pixel_to_world(x, y, 1)
+def pix2coord(wcs, pix_x, pix_y):
+    coord = wcs.pixel_to_world(pix_x, pix_y, 1)
     #print('coord')
     #print(coord)
     return coord[0].ra.deg, coord[0].dec.deg
 
 def compute_inclination(bmaj, bmin, q=0.2):
     cosi = np.sqrt(((bmin/bmaj)**2 - q**2)/(1 - q**2))
-    # From A7) in http://articles.adsabs.harvard.edu/pdf/1992MNRAS.258..334S 
+    # From A7) in http://articles.adsabs.harvard.edu/pdf/1992MNRAS.258..334S
     p = bmin/bmaj
     q = 0.65*p - 0.072*p**3.9
     cosi = np.sqrt(
@@ -61,7 +89,8 @@ def convert_units(raw_cat, fitsfile):
     # Convert x,y in pixels to R.A.,Dec. in deg
     ra_deg, dec_deg = pix2coord(wcs, raw_cat['x'], raw_cat['y'])
     # Get pixel size
-    pix2arcsec = wcs.wcs.get_cdelt()[1]*3600. # This assumes same pixel size in both direction
+    pix2arcsec = wcs.wcs.get_cdelt()[1]*3600. # This assumes same pixel size
+                                              #in both direction
     pix2freq = f[0].header['CDELT3']
     return ra_deg, dec_deg, pix2arcsec,pix2freq
 
@@ -72,7 +101,9 @@ def frequency_to_vel(freq, invert=False):
         return f0*np.sqrt((1-freq/c)/(1+freq/c))
 
 def convert_flux(flux,filename):
-    #This assume that flux comes from SoFiA in Jy/beam and converts it to Jy * km/s base on the header
+    '''This assume that flux comes from SoFiA in Jy/beam and converts it to
+    Jy * km/s base on the header
+    '''
     hdr = fits.getheader(filename)
     print(hdr['BMAJ'],hdr['BMIN'])
     beamarea=(np.pi*abs(hdr['BMAJ']*hdr['BMIN']))/(4.*np.log(2.))
@@ -96,19 +127,21 @@ def convert_frequency_axis(filename, outname, velocity_req = 'radio'):
     crpix = float(hdr['CRPIX3'])
     crval = float(hdr['CRVAL3'])
     naxis_len = float(hdr['NAXIS3'])
-    # make sure the central pixel is rather central else large errors are introduce in both vrad and rel
+    # make sure the central pixel is rather central else large errors
+    # are introduce in both vrad and rel
     if naxis_len/2.-5 < crpix <  naxis_len/2.+5:
-            hdr_wcs = WCS(hdr)
-            centralx,centraly, new_freq = hdr_wcs.pix2world([hdr['CRPIX1'],hdr['CRPIX2'],naxis_len/2.],1)
-            hdr['CRPIX3'] = new_pix
-            crval = new_freq
+        hdr_wcs = WCS(hdr)
+        centralx,centraly, new_freq = hdr_wcs.pix2world([hdr['CRPIX1'], \
+                                              hdr['CRPIX2'],naxis_len/2.],1)
+        hdr['CRPIX3'] = new_pix
+        crval = new_freq
     #Now convert
     if velocity_req == 'radio':
-          # convert from frequency to radio velocity
-            cdelt_vel = -c_ms*float(hdr['CDELT3'])/f0
-            crval_vel = c_ms*(1-crval/f0)
-            # https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
-            hdr['CTYPE3'] = 'VRAD'
+      # convert from frequency to radio velocity
+        cdelt_vel = -c_ms*float(hdr['CDELT3'])/f0
+        crval_vel = c_ms*(1-crval/f0)
+      # https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
+        hdr['CTYPE3'] = 'VRAD'
     elif velocity_req == 'relativistic':
         # This should always only ever be used for cubes with small velocity range
         crval_vel = frequency_to_vel(crval)
@@ -138,7 +171,8 @@ def process_catalog(raw_cat, fitsfile):
     # Unit conversion
     ra_deg, dec_deg, pix2arcsec,pix2freq = convert_units(raw_cat, fitsfile)
     hi_size = raw_cat['ell_maj']*pix2arcsec
-    # Estimate inclination based on fitted ellipsoid, assuming the galaxy is intrinsically circular
+    # Estimate inclination based on fitted ellipsoid, assuming the galaxy is
+    # intrinsically circular
     inclination = compute_inclination(raw_cat['ell_maj'], raw_cat['ell_min'])
 
     # Construct the output catalog
@@ -147,32 +181,45 @@ def process_catalog(raw_cat, fitsfile):
     processed_cat['ra'] = ra_deg
     processed_cat['dec'] = dec_deg
     processed_cat['hi_size'] = hi_size
-    processed_cat['line_flux_integral'] = convert_flux(raw_cat['f_sum'],fitsfile)  # Now converted to Jy*km/s verifcation for developments needed
+    # Now converted to Jy*km/s verifcation for developments needed
+    processed_cat['line_flux_integral'] = convert_flux(raw_cat['f_sum'],\
+                                                       fitsfile)
     if 'freq' in raw_cat:
         processed_cat['central_freq'] =  raw_cat['freq']
         #processed_cat['central_velocity'] = frequency_to_vel(raw_cat['freq'])
-        processed_cat['w20'] = frequency_to_vel(raw_cat['freq']-raw_cat['w20']/2.*pix2freq)-frequency_to_vel(raw_cat['freq']+raw_cat['w20']/2.*pix2freq) # we need to clarify if the units and the definition is the same
+        processed_cat['w20'] = frequency_to_vel(raw_cat['freq']-\
+                               raw_cat['w20']/2.*pix2freq)-\
+                               frequency_to_vel(raw_cat['freq']+\
+                               raw_cat['w20']/2.*pix2freq) # we need to clarify
+                                   # if the units and the definition is the same
     else:
         #processed_cat['central_velocity'] =  raw_cat['v_app']
         if 'v_app' in raw_cat.columns:
             print('Using v_app column')
-            processed_cat['central_freq'] = frequency_to_vel(raw_cat['v_app'],invert=True)
+            processed_cat['central_freq'] = frequency_to_vel(raw_cat['v_app'],\
+                                                             invert=True)
         elif 'freq' in raw_cat.columns:
             print('Using freq column')
             processed_cat['central_freq'] = raw_cat['freq']
-        elif 'v_opt' in raw_cat.columns:  # This case should not be included for production, just to test the minimal cube
+        # This case should not be included for production,
+        # just to test the minimal cube
+        elif 'v_opt' in raw_cat.columns:
             print('WARNING. Using v_opt column. Use only to check the workflow')
-            processed_cat['central_freq'] = frequency_to_vel(raw_cat['v_opt'],invert=True)
+            processed_cat['central_freq'] = frequency_to_vel(raw_cat['v_opt'],\
+                                                             invert=True)
         processed_cat['w20'] = raw_cat['w20']*pix2freq
          # we need to clarify if what sofia gives is the central freq
     processed_cat['w20'] *= 1e-3 # To convert from m/s to km/s
-    processed_cat['pa'] = raw_cat['kin_pa']  # we need to clarify if Sofia kinematic angle agrees with their P.A.
+    # we need to clarify if Sofia kinematic angle agrees with their P.A.
+    processed_cat['pa'] = raw_cat['kin_pa']
     processed_cat['i'] = inclination
     processed_cat['rms'] = raw_cat['rms']
     processed_cat['subcube'] = os.path.basename(fitsfile).split('_')[1].split('.fits')[0]
     processed_cat.reset_index(drop=True, inplace=True)
     # This is just to set the right order of the output columns
-    processed_cat = processed_cat[['id_subcube', 'ra', 'dec', 'hi_size', 'line_flux_integral', 'central_freq', 'pa', 'i', 'w20', 'rms', 'subcube']]
+    processed_cat = processed_cat[['id_subcube', 'ra', 'dec', 'hi_size', \
+                                   'line_flux_integral', 'central_freq', 'pa', \
+                                   'i', 'w20', 'rms', 'subcube']]
     processed_cat['central_freq'] = processed_cat['central_freq'].map('{:.1f}'.format)
     return processed_cat
 
@@ -201,4 +248,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
