52a830ec1887e5ebd5a494474c5a50e4bdf15d2d
jmoldon <jmoldon@iaa.es>
2021-08-06 10:43:08 +0200

    include extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'J.Moldón, L.Darriba, et al.'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.coverage','sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.coverage','sphinx.ext.napoleon', 'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
